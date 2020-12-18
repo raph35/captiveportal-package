c597ab6b87de77b0c58cf5494caa979448696a11
Raphael RALAIKOA <raph35ral@gmail.com>
2020-12-18 05:28:23 +0300

    Changing gitignore file and fixing bug on daemon captiveportal(making the daemon sleep in an infinite loop)

--- lib/addUser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Adding mac to the iptables chain
-# echo "Excluding ip: $1"
-
-/sbin/iptables -t mangle -I internet -m mac --mac-source $1 -j RETURN
--- src/function.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
-
-#include <iostream>
-
-void printSplashScreen();
-void printEndProgrammText();
-
-#endif // FUNCTION_H
